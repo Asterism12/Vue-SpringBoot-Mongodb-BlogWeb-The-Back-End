7e6da9a0ed3587ce0836d507e7c45c9f675b1ccb
ljxfai <18935174185@163.com>
2019-08-25 14:59:21 +0800

    success

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,27 +1,29 @@
 package com.example.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import com.example.beans.User;
 import com.example.mongodb.MongodbController;
 import com.example.result.Result;
 
 //与前端交互，从api/login接受前端信息，使用Result类返回信息
 
-@Controller
+@RestController
 public class LoginController {
 	
 	@CrossOrigin
 	@PostMapping(value="/login")
 	@ResponseBody
 	public Result login(@RequestBody User requestUser) {
-		System.out.println("hello world");
-		String username=requestUser.getName();
+		String username=requestUser.getUsername();
 		username=HtmlUtils.htmlEscape(username);
 		System.out.println(username);
 		if(MongodbController.login(username,requestUser.getPassword())) {
@@ -32,5 +34,10 @@ public class LoginController {
 			System.out.println("test");
 			return new Result(400);
 		}
+	}
+	
+	@RequestMapping("hello")
+	public String hello() {
+		return "hello";
 	}
 }
