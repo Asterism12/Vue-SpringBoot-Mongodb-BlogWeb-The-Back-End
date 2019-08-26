9f2a3ab9baf1be8bc5050b0500328e7a2887856a
louix66 <05018673c18010e81f58c00fd9e1e474@huawei.com>
2019-08-26 11:00:28 +0800

    123

--- src/main/java/com/example/controller/CommentsController.java
+++ src/main/java/com/example/controller/CommentsController.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
+import com.example.beans.Blog;
+import com.example.beans.Comments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class CommentsController {
@@ -14,7 +19,16 @@ public class CommentsController {
     @GetMapping(value="/comment")
     @ResponseBody
     //发布评论
-    public void publishComment(String username,String comment) {
+    public void publishComment(int id,String username,String comment)
+    {
+        Query query = new Query();
+        username= HtmlUtils.htmlEscape(username);
+        comment=HtmlUtils.htmlEscape(comment);
+        Blog blog=mongotemplate.findOne(query.addCriteria(Criteria.where("id").is(id)),Blog.class);
+        Comments comments = new Comments();
+        comments.setContent(comment);
+        comments.setUsername(username);
+        blog.writeComments(comments);
     }
 
 
