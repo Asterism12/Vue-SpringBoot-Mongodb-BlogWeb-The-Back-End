f475721a43d177ae7b44bfec243c5f1f9a99c144
louix66 <05018673c18010e81f58c00fd9e1e474@huawei.com>
2019-08-29 15:12:34 +0800

    1

--- src/main/java/com/example/beans/Blog.java
+++ src/main/java/com/example/beans/Blog.java
@@ -103,23 +103,6 @@ public class Blog extends Entity{
 		commentsArrayList.remove(comments);
 	}
 
-   /* //搜索博文题目
-    public List<Blog> searchTitle(String keyword,Integer code)
-    {
-        Pattern pattern = Pattern.compile("^.*+keyword+.*$",Pattern.CASE_INSENSITIVE);//???
-        Query query = new Query(Criteria.where("title").regex(pattern).and("code").is(code));
-        List<Blog> resault = mongotemplate.find(query,Blog.class,"blog");
-        return resault;
-    }
-
-    //搜索博文内容
-	public List<Blog> searchContent(String keyword,Integer code)
-	{
-		Pattern pattern = Pattern.compile("^.*+keyword+.*$",Pattern.CASE_INSENSITIVE);//???
-		Query query = new Query(Criteria.where("Content").regex(pattern).and("code").is(code));
-		List<Blog> resault = mongotemplate.find(query,Blog.class,"blog");
-		return resault;
-	}*/
-
+ 
 
 }
