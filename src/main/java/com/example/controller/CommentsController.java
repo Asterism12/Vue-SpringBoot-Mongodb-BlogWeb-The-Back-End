91107099672cb412e8d58ede64cfec2ac18a2492
ljxfai <04fea562a68010e11f87c0109aeb246a@huawei.com>
2019-08-29 16:56:02 +0800

    llllll

--- src/main/java/com/example/controller/CommentsController.java
+++ src/main/java/com/example/controller/CommentsController.java
@@ -24,11 +24,13 @@ public class CommentsController {
     public Result publishComment(@RequestParam(value="username") String username, @RequestParam(value="bid") int id, @RequestParam(value="content")String comment)
     {
       	System.out.println("发布评论 "+username+" "+id+" "+comment);
-      
+      	System.out.println(mongotemplate);
+      	System.out.println("LLLLLL");
         Query query = new Query();
         username= HtmlUtils.htmlEscape(username);
         comment=HtmlUtils.htmlEscape(comment);
         Blog blog=mongotemplate.findOne(query.addCriteria(Criteria.where("bid").is(id)),Blog.class);
+      
         if(blog != null)
         {
             Comments comments = new Comments();
